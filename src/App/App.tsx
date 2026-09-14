a9df4c7aa428aef792dd9bfde161ac80e86bed19
Patrik Nusszer <patrik.nusszer@icloud.com>
2026-09-14 14:34:23 +0200

    Add GitHub Actions workflow for deployment

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom"
 import Navbar from '../Navbar/Navbar'
 import Home from '../Home/Home'
 import Blog from '../Blog'
@@ -12,7 +12,7 @@ import { fejerRoute } from "../Articles/Routes"
 
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
 <link href="https://fonts.googleapis.com/css2?family=Anonymous+Pro:ital,wght@0,400;0,700;1,400;1,700&family=Architects+Daughter&family=Cabin+Sketch:wght@400;700&family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&family=Creepster&family=Cutive+Mono&family=Ewert&family=Fascinate&family=Fredericka+the+Great&family=Fuzzy+Bubbles:wght@400;700&family=Handlee&family=IM+Fell+English:ital@0;1&family=Kranky&family=Limelight&family=Miltonian&family=Neucha&family=Nixie+One&family=Ole&family=Rye&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Special+Elite&family=Walter+Turncoat&display=swap" rel="stylesheet"></link>
 <div id="background" />
         <Navbar/>
@@ -26,7 +26,7 @@ function App() {
             <Route {...fejerRoute}></Route>
           </Routes>
         </div>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
